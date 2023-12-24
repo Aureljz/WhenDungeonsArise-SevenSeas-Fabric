4844496664eb0b50230317aec307bacc5e05c723
Aureljz <catking060@gmail.com>
2023-12-24 17:50:07 -0300

    Initial commit

---
+++ src/client/java/net/aurelj/dungeons_arise_seven_seas/DungeonsAriseSevenSeasClient.java
@@ -0,0 +1,10 @@
+package net.aurelj.dungeons_arise_seven_seas;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DungeonsAriseSevenSeasClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
